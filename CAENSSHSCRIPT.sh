14e873d35642d791ecf6871424d65ac9fb1507db
zjyin <zjyin@umich.edu>
2020-01-26 18:50:04 -0500

    Initial Comit

---
+++ CAENSSHSCRIPT.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/expect -f
+
+#I installed- sudo apt-get install expect
+#https://hostadvice.com/how-to/how-to-automate-tasks-in-ssh/
+#to compile, NOT chmod +x fileName
+#its chmod 755 fileName
+#In short, run ssh with the -q flag to disable warnings/diagnostics (but not errors).
+
+spawn ssh zjyin@login.engin.umich.edu
+expect "*: "
+send "insertpasswordhere\r"
+expect "4): "
+send "1\r"
+interact
+
+
+#1) To allow this script to be executed anywhere, create an alias in bashrc
+#) cd ~
+#) ls -alias
+#) vim .bashrc
+#) got to the very bottom of the file, press i for insert mode
+#) alias	 shortcutName="insertPathToScript/./scriptName_script.sh" example: alias sshcaen="/mnt/c/A_scripts/SSH_caen_script.sh"
+#) Press the ESC key
+#) :wq
+#) Reload the terminal, try out your new alias command: shortCutName
